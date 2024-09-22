22e9b58e954eed16cf2699cae8cb7f517c172b42
Ryan Dempsey <rjd9896@gmail.com>
2024-09-22 23:43:27 +0900

    Update movie.js

--- src/movie.js
+++ src/movie.js
@@ -16,7 +16,7 @@ export const movieList = [
     {
         id: 3,
         title: "Straight outta Compton",
-        genre: "",
+        genre: "Drama",
         image: ,
         rating: ,
     },
