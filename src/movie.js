a4210a1aafb9e3d7e46d2f4c083ab86b88350574
Ryan Dempsey <rjd9896@gmail.com>
2024-09-22 23:33:46 +0900

    Update movie.js

--- src/movie.js
+++ src/movie.js
@@ -132,4 +132,11 @@ export const movieList = [
         image: ,
         rating: ,
     },
+    {
+        id: 10,
+        title: ,
+        genre: ,
+        image: ,
+        rating: ,
+    },
 ]
